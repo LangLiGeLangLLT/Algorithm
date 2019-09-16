1e8fb35e947ce75f8085dbc843dfdc4b14e0f1e1
LiangTong <LiangTong@DESKTOP-4RBM2P6>
2019-09-16 16:36:04 +0800

    添加快速排序

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -1,39 +1,41 @@
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 
 using namespace std;
 
+// 每轮默认以第一个元素为基准，若原始数组有序，则时间复杂度为 O(n^2)
+
 void QuickSort(vector<int>& nums, int l, int r)
 {
-	if (r - l <= 1) // 分到剩余 1 个元素为止
+	if (l >= r) // 分到剩余 1 个元素为止
 		return;
 
-	int i = l, j = r - 1, key = nums[i];
+	int i = l, j = r, key = nums[i];
 
 	while (i < j)
 	{
-		while (i < j && nums[j] >= key)
-			--j;
+		while (i < j && nums[j] >= key) --j;
 		nums[i] = nums[j];
 
-		while (i < j && nums[i] <= key)
-			++i;
+		while (i < j && nums[i] <= key) ++i;
 		nums[j] = nums[i];
 	}
 	nums[i] = key;
 
-	QuickSort(nums, l, i);
+	QuickSort(nums, l, i - 1);
 	QuickSort(nums, i + 1, r);
 }
 
 int main()
 {
-	vector<int> nums = { 9,1,4,2,6 };
+	vector<int> nums = { 2, 1, 7, 9, 5, 8 };
 
-	QuickSort(nums, 0, nums.size());
+	for (int num : nums) cout << num << " ";
 
-	for (int i = 0; i < nums.size(); ++i)
-		cout << nums[i] << " ";
+	QuickSort(nums, 0, nums.size() - 1);
+
+	cout << endl;
+	for (int num : nums) cout << num << " ";
 
 	return 0;
 }
